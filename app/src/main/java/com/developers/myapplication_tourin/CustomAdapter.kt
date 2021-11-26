bdc607691d7dcc7c1b41005bcbbc0565241389e9
John Doe <dec1984@msn.com>
2021-11-25 21:56:07 -0500

    interface recyclerView

---
+++ app/src/main/java/com/developers/myapplication_tourin/CustomAdapter.kt
@@ -0,0 +1,39 @@
+package com.developers.myapplication_tourin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    private val titles = arrayOf("Monserrate",
+    "Plaza de Bolivar",
+    "Museo de Botero")
+
+    private val images = intArrayOf(R.drawable.ic_monserrate,
+        R.drawable.ic_plazabolibar,
+        R.drawable.ic_museobotero)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemTitle.text = titles[i]
+        viewHolder.itemImage.setImageResource(images[i])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView = itemView.findViewById(R.id.item_image)
+        var itemTitle: TextView = itemView.findViewById(R.id.item_title)
+
+    }
+}
